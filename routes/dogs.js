22c9eed572cde1919adb9fa7113b7c28e0dc6586
Ramu Devarashetti <98135813+RamuDevarashetti1@users.noreply.github.com>
2022-04-13 19:23:18 -0500

    Modified url

--- routes/dogs.js
+++ routes/dogs.js
@@ -2,8 +2,15 @@ var express = require('express');
 const dog_controller = require('../controllers/dog');
 var router = express.Router();
 
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.get('dogs', { title: 'Search Results' });
+// });
+
 
 /* GET costumes */
+
 router.get('/', dog_controller.dog_view_all_Page );
 
 module.exports = router;
+
