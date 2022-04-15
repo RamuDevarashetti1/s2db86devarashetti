6a146896c1ce3526d52bf60bf4a7710f1aa29d31
Ramu Devarashetti <98135813+RamuDevarashetti1@users.noreply.github.com>
2022-04-15 01:13:45 -0500

    Added PUT and GET by Id Routes for Dog objects

--- controllers/dog.js
+++ controllers/dog.js
@@ -11,9 +11,6 @@ exports.dog_list = async function (req, res) {
     }
 };
 
-exports.dog_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Dog detail: ' + req.params.id);
-};
 
 exports.dog_create_post = async function (req, res) {
     console.log(req.body)
@@ -35,9 +32,6 @@ exports.dog_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Dog delete DELETE ' + req.params.id);
 };
 
-exports.dog_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Dog update PUT' + req.params.id);
-};
 
 
 exports.dog_view_all_Page = async function (req, res) {
@@ -48,5 +42,38 @@ exports.dog_view_all_Page = async function (req, res) {
     catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
+    }
+};
+
+// for a specific Dog.
+exports.dog_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Dog.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+
+// Handle Dog update form on PUT.
+exports.dog_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Dog.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.dog_bread)
+    toUpdate.dog_bread = req.body.dog_bread;
+    if(req.body.dog_cost) toUpdate.dog_cost = req.body.dog_cost;
+    if(req.body.dog_color) toUpdate.dog_color = req.body.dog_color;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
     }
 };
