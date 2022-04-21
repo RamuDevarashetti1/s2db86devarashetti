f4b344c14644ceae3d260ac697121ceb3a5e7cdb
Ramu Devarashetti <98135813+RamuDevarashetti1@users.noreply.github.com>
2022-04-20 20:00:03 -0500

    Added routes

--- routes/dogs.js
+++ routes/dogs.js
@@ -11,6 +11,14 @@ var router = express.Router();
 /* GET costumes */
 
 router.get('/', dog_controller.dog_view_all_Page );
+/* GET detail dog page */
+router.get('/detail', dog_controller.dog_view_one_Page);
+/* GET create dog page */
+router.get('/create', dog_controller.dog_create_Page);
+/* GET create update page */
+router.get('/update', dog_controller.dog_update_Page);
+/* GET delete dog page */
+router.get('/delete', dog_controller.dog_delete_Page);
 
 module.exports = router;
 
