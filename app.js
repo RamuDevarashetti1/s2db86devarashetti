0a561fa4ead3fd8e96aef1a363bc53e045f73b43
Ramu Devarashetti <98135813+RamuDevarashetti1@users.noreply.github.com>
2022-04-22 09:48:00 -0500

    Added Auth related code

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+    if (err) { return done(err); }
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (!user.validPassword(password)) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+      return done(null, user);
+    }
+  );
 
 const connectionString = "mongodb+srv://admin:admin@cluster0.hxyu1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose = require('mongoose');
@@ -37,6 +53,14 @@ app.use('/dogs', dogsRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter)
 app.use('/resource', resourceRouter);
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
