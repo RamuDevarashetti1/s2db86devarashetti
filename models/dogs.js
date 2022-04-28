300066b11274c9f252213ed3bfe1630339100f94
Ramu Devarashetti <98135813+RamuDevarashetti1@users.noreply.github.com>
2022-04-27 23:14:52 -0500

    Added Models

--- models/dogs.js
+++ models/dogs.js
@@ -1,7 +1,20 @@
 const mongoose = require("mongoose")
 const dogSchema = mongoose.Schema({
-    dog_bread: String,
-    dog_color: String,
-    dog_cost: Number
+    dog_bread: { 
+        type: String,
+        minLength: 2,
+        maxLength: 25, 
+        required: true,
+        trim: true
+    },
+    dog_color: { 
+        type: String, 
+        required: true
+    },
+    dog_cost:{ 
+        type: Number, 
+        min:[10, 'Minimum of 10$ but, got {VALUE}'], 
+        max:2000
+    }
 })
 module.exports = mongoose.model("Dog", dogSchema)
